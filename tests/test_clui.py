9d87519c2bd754277f7e21842dc4441edc21d281
Henrique Leal <hm.leal@hotmail.com>
2017-07-10 16:54:07 -0300

    test: Implement some tests

---
+++ tests/test_clui.py
@@ -0,0 +1,12 @@
+import unittest
+
+from py_clui import gauge
+
+
+class TestSpinner(unittest.TestCase):
+    raise NotImplementedError
+
+
+class TestGauge(unittest.TestCase):
+    def test_empty_max_value_gauge(self):
+        self.assertEqual('[]', gauge(10, 0, 100, 80))
